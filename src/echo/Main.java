97eea8f4389af830a0349dc72cd22ed74180c1bc
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-12-09 11:46:45 -0300

    echo

---
+++ src/echo/Main.java
@@ -0,0 +1,14 @@
+package echo;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) throws java.lang.Exception {
+
+      Scanner sc = new Scanner(System.in);
+      int aNumber = sc.nextInt();
+      System.out.println(aNumber);
+      
+      sc.close();
+	}
+}
