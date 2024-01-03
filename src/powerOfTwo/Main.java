8b53bb345746e2c1b40412153d79737868c12f3d
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-01-03 12:08:39 -0300

    adding the resolution of the challenge to powers of two

--- src/powerOfTwo/Main.java
+++ src/powerOfTwo/Main.java
@@ -1,5 +1,17 @@
 package powerOfTwo;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 1;
+
+		for (int i = 0; i < n; i++) {
+			result *= 2;
+		}
+		System.out.println(result);
+		sc.close();
+	}
 }
