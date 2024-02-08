ebf02ea793397c95975ee39a7b209cf0b4055b00
Gustavo Rodrigues <Gustavorr001@gmail.com>
2024-02-08 11:43:41 -0300

    adding a solution to the challenge of calculating double numbers

---
+++ src/calculatingTwiceTheNumber/Main.java
@@ -0,0 +1,12 @@
+package calculatingTwiceTheNumber;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws java.lang.Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n * 2);
+		sc.close();
+	}
+}
